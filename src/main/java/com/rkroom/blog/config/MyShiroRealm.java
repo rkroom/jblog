7ce28acfdd34ffa4932ad7c14136ff6b8de4b3b4
rkroom <NTM2NzI0MzUrcmtyb29tQHVzZXJzLm5vcmVwbHkuZ2l0aHViLmNvbQ==>
2019-11-22 13:23:08 +0800

    delete integration web file
    delete integration web file

--- src/main/java/com/rkroom/blog/config/MyShiroRealm.java
+++ src/main/java/com/rkroom/blog/config/MyShiroRealm.java
@@ -38,19 +38,6 @@ public class MyShiroRealm extends AuthorizingRealm { //继承自AuthorizingRealm
     @Override //重写doGetAuthenticationInfo方法
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token)
             throws AuthenticationException {
-        //如果token是UsernamePasswordToken的实例，则使用session
-        //此处是为了兼容前后端不分离的老版程序而存在的
-        if (token instanceof UsernamePasswordToken) {
-            String principal = (String) token.getPrincipal(); //获取传递过来的用户名
-            User user = Optional.ofNullable(userService.selectByUsername(principal)).orElseThrow(UnknownAccountException::new); //获取对应user的相关信息
-            if (user.isLocked()) { //如果用户被锁定
-                throw new LockedAccountException(); //抛出锁定信息
-            }
-            SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(principal, user.getPassword(), getName()); //对比密码
-            Session session = SecurityUtils.getSubject().getSession(); //获取session
-            ((org.apache.shiro.session.Session) session).setAttribute("USER_SESSION", user); //设置session
-            return authenticationInfo;
-        } else {
             //使用JWT的token进行登陆
             //获取token
             String authuser = (String) token.getCredentials();
@@ -71,7 +58,6 @@ public class MyShiroRealm extends AuthorizingRealm { //继承自AuthorizingRealm
             }
             //返回验证信息
             return new SimpleAuthenticationInfo(authuser, authuser, "my_realm");
-        }
     }
 
     @Override //重写doGetAuthorizationInfo方法，doGetAuthorizationInfo方法主要是获取用户权限的，我们暂时不涉及这个方面的内容
--- src/main/java/com/rkroom/blog/config/ShiroConfig.java
+++ src/main/java/com/rkroom/blog/config/ShiroConfig.java
@@ -1,12 +1,9 @@
 package com.rkroom.blog.config;
 
 import org.apache.shiro.cache.ehcache.EhCacheManager;
-import org.apache.shiro.codec.Base64;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.apache.shiro.web.servlet.SimpleCookie;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,29 +15,7 @@ import javax.servlet.Filter;
 
 @Configuration //定义配置类
 public class ShiroConfig {
-    @Bean//设置cookie
-    public SimpleCookie rememberMeCookie(){
-        SimpleCookie simpleCookie = new SimpleCookie("rememberMe"); //cookie为rememberMe
-        simpleCookie.setHttpOnly(true); //仅限http
-        simpleCookie.setPath("/"); //设置作用范围
-        simpleCookie.setMaxAge(604800); //设置生效时常，以秒为单位，这里是七天
-        return simpleCookie;
-    }
 
-    @Bean //设置rememberme管理
-    public CookieRememberMeManager rememberMeManager() {
-        CookieRememberMeManager cookieRememberMeManager = new CookieRememberMeManager();
-        cookieRememberMeManager.setCookie(rememberMeCookie());
-        /* key生成的方法，建议自行生成key保证服务的安全性。
-        try{
-        KeyGenerator keygen = KeyGenerator.getInstance("AES");
-           SecretKey deskey = keygen.generateKey();
-           System.out.println(Base64.encodeToString(deskey.getEncoded()));}catch (Exception e) {
-           System.out.println("exception:"+e.toString());
-        }*/
-        cookieRememberMeManager.setCipherKey(Base64.decode("S8+74LJR9whLdemqJbJeeA=="));
-        return cookieRememberMeManager;
-    }
     @Bean //设置缓存bean
     public EhCacheManager getEhCacheManager() {
         EhCacheManager em = new EhCacheManager();
@@ -83,9 +58,6 @@ public class ShiroConfig {
         filterMap.put("jwt", new JWTFilter());
         shiroFilterFactoryBean.setFilters(filterMap);
         shiroFilterFactoryBean.setSecurityManager(securityManager);
-        shiroFilterFactoryBean.setLoginUrl("/login"); //登陆页面的URL
-        shiroFilterFactoryBean.setSuccessUrl("/admin/articlepost"); //登陆成功后跳转的URL
-        shiroFilterFactoryBean.setUnauthorizedUrl("/denied"); //禁止登陆的URL
         loadShiroFilterChain(shiroFilterFactoryBean); //加载过滤链
         return shiroFilterFactoryBean;
     }
@@ -95,8 +67,6 @@ public class ShiroConfig {
         Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
         // 使/api/路径下所有的请求都接受JWT的过滤
         filterChainDefinitionMap.put("/api/**","jwt");
-        filterChainDefinitionMap.put("/admin/**","user");
-        filterChainDefinitionMap.put("/login", "authc");
         filterChainDefinitionMap.put("/**", "anon");
         shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
     }
